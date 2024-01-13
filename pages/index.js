d0d5a54918bab5630b885dc8c68d590f0ac64e97
Callista <callyjchan@gmail.com>
2024-01-12 23:50:50 -0800

    Done

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import Form from '@/Instagram-login-layout/instagram-login-page/components/Form/index/js'
+import Form from '@/Instagram-login-layout/instagram-login-page/components/Form/index.js'
 import Footer from '@/Instagram-login-layout/instagram-login-page/components/Footer/index.js'
 
 
